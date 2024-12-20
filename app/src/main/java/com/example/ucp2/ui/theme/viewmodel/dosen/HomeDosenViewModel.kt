e2857d7fc943966a03b504488b02a29bb60b0d67
mutiara126 <salsamutiara1812@gmail.com>
2024-12-20 23:08:55 +0700

    mengelola data terkait dosen dari repository

---
+++ app/src/main/java/com/example/ucp2/ui/theme/viewmodel/dosen/HomeDosenViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.ucp2.ui.theme.viewmodel.dosen
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.Data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeDosenViewModel(
+    private val repositoryDosen: RepositoryDosen
+
+) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> = repositoryDosen.getAllDosen()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listDosen = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
